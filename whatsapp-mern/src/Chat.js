d7bc86f8c82802bc6b069f06fa1ce9a44482e075
VCRA\ppattepu <ppattepu@vocera.com>
2021-05-20 20:23:37 +0530

    Complete Fromt end

--- whatsapp-mern/src/Chat.js
+++ whatsapp-mern/src/Chat.js
@@ -1,9 +1,56 @@
+import { Avatar,IconButton } from '@material-ui/core';
 import React from 'react'
 import './Chat.css';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search';
+import { InsertEmoticon, Mic } from '@material-ui/icons';
+
 function Chat() {
     return (
         <div className="chat">
-            <h1>I am a chat</h1>
+            <div className="chat_header">
+                <Avatar />
+                <div className="chat__headerInfo">
+                    <h3>Room Name</h3>
+                    <p>Last seen at ...</p>
+                </div>
+
+                <div className="chat__headerRight">
+                    <IconButton>
+                        <SearchIcon />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFileIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+
+            <div className="chat__body">
+                <p className="chat__message">
+                    <span className="chat__name">Pavan</span>
+                    This is a message
+                    <span className="chat__timestamp">{new Date().toUTCString()}</span>
+                </p>
+                <p className="chat__message chat__receiver">
+                    <span className="chat__name">Pavan</span>
+                    This is a message
+                    <span className="chat__timestamp">{new Date().toUTCString()}</span>
+                </p>
+
+            </div>
+            <div className="chat__footer">
+                <InsertEmoticon />
+                <form>
+                    <input placeholder="type a message" type="text" />
+                    <button type="submit">Send a message</button>
+
+                </form>
+                <Mic />
+            </div>
         </div>
     )
 }
