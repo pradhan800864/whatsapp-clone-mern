e234f55568b7f3bd74448b82a77c5d702660770f
VCRA\ppattepu <ppattepu@vocera.com>
2021-05-20 17:50:03 +0530

    firebase Setup

---
+++ whatsapp-mern/src/firebase.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAmeuUMebtZo9O9W0K4KZnZAoXgjvge2VE",
+    authDomain: "whatsapp-mern-725d1.firebaseapp.com",
+    projectId: "whatsapp-mern-725d1",
+    storageBucket: "whatsapp-mern-725d1.appspot.com",
+    messagingSenderId: "260316740844",
+    appId: "1:260316740844:web:606d93c00c7b1e5e89b9c9",
+    measurementId: "G-WFF8M5ZYP9"
+  };
+
+  export default firebaseConfig;
